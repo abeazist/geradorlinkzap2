cf258fd9831e24bd4e0ddb21c3eee29f3a048301
beatriz <128046453+abeazist@users.noreply.github.com>
2024-05-14 00:00:20 -0300

    mascara feita

---
+++ parte.js
@@ -0,0 +1,21 @@
+const telefoneInput = document.getElementById('telefone');
+const link = document.querySelectorAll('link')
+const enviar = document.querySelectorAll('enviar')
+
+
+telefoneInput.addEventListener('input',() => {
+    var inputValue = target.value.replace(/\D/g, ''); 
+    var formattedValue = '';
+
+    if (inputValue.length > 0) {
+        formattedValue = '(' + inputValue.substring(0, 2);
+    }
+    if (inputValue.length > 2) {
+        formattedValue += ') ' + inputValue.substring(2, 7);
+    }
+    if (inputValue.length > 6) {
+        formattedValue += '-' + inputValue.substring(7, 11);
+    }
+
+    target.value = formattedValue;
+});
