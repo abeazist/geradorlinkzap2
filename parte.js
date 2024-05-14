0e9848d24b84fcaf3c32dfd2f2ccbd7595ebaee5
beatriz <128046453+abeazist@users.noreply.github.com>
2024-05-14 16:14:43 -0300

    A ARROW FUNCTION TAVA ERRADAAAAAAAAAAAAAAAAAAA

--- parte.js
+++ parte.js
@@ -1,26 +1,33 @@
 const telefoneInput = document.getElementById('telefone');
-const link = document.querySelectorAll('link')
-const enviar = document.querySelectorAll('enviar')
-
-
-telefoneInput.addEventListener('input',() => {
-    var inputValue = target.value.replace(/\D/g, ''); 
-    var formattedValue = '';
-
-    if (inputValue.length > 0) {
-        formattedValue = '(' + inputValue.substring(0, 2);
-    }
-    if (inputValue.length > 2) {
-        formattedValue += ') ' + inputValue.substring(2, 7);
-    }
-    if (inputValue.length > 6) {
-        formattedValue += '-' + inputValue.substring(7, 11);
-    }
-
-    target.value = formattedValue;
-    target.style.classList.add('green-text');
+const linkzap = document.querySelectorAll('linkzap');
+const enviar = document.querySelectorAll('enviar');
 
 
 
-});
+telefoneInput.addEventListener('input',() => { 
+var inputValue = event.target.value.replace(/\D/g, ''); 
+var formattedValue = ''; 
 
+if (inputValue.length > 0) { 
+    formattedValue = '(' + inputValue.substring(0, 2); 
+
+} 
+if (inputValue.length > 2) { 
+    formattedValue += ') ' + inputValue.substring(2, 7); 
+
+} 
+if (inputValue.length > 6) { 
+    formattedValue += '-' + inputValue.substring(7, 11);
+
+} 
+    
+event.target.value = formattedValue
+//telefoneInput.style.color='green'
+
+ });
+ 
+ linkzap.addEventListener('click',() => {
+    var 
+ })
+ 
+ 
